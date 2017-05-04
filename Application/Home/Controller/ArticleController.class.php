e7452010568df809f3082db57d4ffc9383d42381
Liangwt <502472460@qq.com>
2017-05-04 06:04:30 -0700

    修改文章页权限，所有人都可以看到公开文章而不需要登录

--- Application/Home/Controller/ArticleController.class.php
+++ Application/Home/Controller/ArticleController.class.php
@@ -148,45 +148,6 @@ class ArticleController extends CommonController{
         }	
 	}
 
-	/**
-	 * 显示单个文章内容
-	 * @param  int    $id 文章id
-	 * @return [type]     [description]
-	 */
-	public function detailArticle($id){	
-        if(!empty($id)){
-            //记录访问信息，5分钟之外访问记录
-            recodeView($id);
-            //展示文章信息
-            $article = D("ArticleView")->where("article.id=".$id)->find();//文章信息
-            $comment = $this->showComment($id);
-            $tags    = M("tag") -> where("article_id=".$id) -> select();//标签信息
-            $this -> assign([
-                "list"            => $article,
-                "classificationL" => $this->classificationL,
-                "comment"         => $comment,
-                "tags"            => $tags,
-            ]);
-            //检测是否有读取这篇文章的权限
-            //当作者为文章作者时，可以编辑
-            if($article["uid"]==$_SESSION["uid"]){
-                $this -> display(T("Home@Article/detailArticle_private"));
-            }elseif ($article["uid"]!=$_SESSION["uid"] && $article["public"]==1){
-                //当不是作者但是文章为公开的时候，可读但是不可写
-                $this -> display(T("Home@Article/detailArticle_public"));
-            }else{
-                //当既不是作者文章又不是公开的时候，报错
-                E("error");
-            }
-        }else{
-            E("error");
-        }
-		
-/*		print_r($tags);
-		exit();*/
-
-	}
-
     /**
      * 改变文章的公开状态
      */
@@ -209,29 +170,6 @@ class ArticleController extends CommonController{
         }
         echo json_encode($response);
     }
-	/**
-	 * 读取评论，通过读取根评论来获得跟评论下面的所有评论
-	 * @param  integer $articleId   文章id
-	 * @return array   $rootComment 评论数组
-	 */
-	private function showComment($articleId=0){
-		$commentWhere = array(
-			"article_id" => $articleId,
-			"comment_pid" => 0,
-			);
-		$rootComment = M("comment")->where($commentWhere)->order("time")->select();//根评论信息
-		foreach ($rootComment as $key => $value) {
-			$commentWhere = array(
-				"article_id" => $articleId,
-				"comment_rid" => $value["id"],
-				);
-			//获得二级评论，键值为根评论
-			$rootComment[$key]["secondComment"] = M("comment")->where($commentWhere)->order("time")->select();
-		}
-		return $rootComment;
-/*		print_r($rootComment); 
-		print_r($secondComment);*/
-	}
 	/**
 	 * 处理添加分类的ajax 
 	 * @return [json] [description]
