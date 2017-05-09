79d97c211a0dc362a69a84b2914afe4aac7a399e
Liangwt <502472460@qq.com>
2017-05-09 07:35:18 -0700

    a little adjust

--- Public/Js/detailArticle.js
+++ Public/Js/detailArticle.js
@@ -3,7 +3,7 @@ $(function(){
 	//为标签附颜色
 	var tagClass = ["label-default","label-primary","label-success","label-warning","label-danger","label-info"];
 	
-	$(".blog-post-meta > span").each(function(i){
+	$("div.blog-post-meta > span").each(function(i){
 		var index = Math.floor(Math.random()*tagClass.length);
 		$(this).addClass(tagClass[index]);
 	})
